1913a18f81b90122abbdb52814425a55c0cf4cb3
Nilay Pagnis <61799574+nilay-pagnis@users.noreply.github.com>
2023-04-15 16:05:46 +0530

    pokedex for pokemons

---
+++ pages/details/[id].js
@@ -0,0 +1,52 @@
+// import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+// import Link from "next/link";
+
+// export default function Details() {
+//   return <h1>Todo</h1>;
+// }
+
+// export async function getStaticPaths() {
+//   const client = new ApolloClient({
+//     uri: "https://graphql-pokemon2.vercel.app",
+//     cache: new InMemoryCache(),
+//   });
+
+//   const result = client.query.toString(({
+//     query: gql`
+//       query pokemons {
+//         pokemons(first: 100) {
+//           id
+//           number
+//           name
+//           weight {
+//             minimum
+//             maximum
+//           }
+//           height {
+//             minimum
+//             maximum
+//           }
+//           classification
+//           types
+//           resistant
+//           weaknesses
+//           fleeRate
+//           maxCP
+//           maxHP
+//           image
+//         }
+//       }
+//     `,
+//   }));
+//   const {data} =  result;
+//   const pokemonsList = data.pokemons;
+//   const paths = pokemonsList.map(({ pokemon }) => {
+//     return {
+//       params: { id: pokemon },
+//     };
+//   });
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
